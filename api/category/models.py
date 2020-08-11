632956ad09634cab059d12564b8a10a7972cc866
Emma Paris <aswinajraj@gmail.com>
2020-08-11 22:51:32 +0530

    new class Category created and registered

--- api/category/models.py
+++ api/category/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=250)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+
+#TO DO
